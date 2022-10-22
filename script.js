6b7a0981e6b935bec05e9874b6add3ee375ed304
Elkurud <samukama18@gmail.com>
2022-10-22 14:37:22 -0300

    funcao embaralhar funcionando

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 let contador = 0
+const card = document.querySelector(".ex");
+
+function distribuir(num){
+    
+    const deck = document.querySelector(".deck")
+    deck.innerHTML += card.innerHTML
+    console.log(deck);
+}
 
 function virarCarta(carta) {
 
@@ -12,6 +20,8 @@ function virarCarta(carta) {
     }
 }
 
+
+
 function desvirar(){
 
     const cartas = document.querySelectorAll('.virada');
@@ -20,3 +30,34 @@ function desvirar(){
         cartas[i].classList.remove('virada')
     }
 }
+
+/*https://codepen.io/dimayakovlev/pen/VmxOYM*/
+function shuffle(elems) {
+ 
+    allElems = (function(){
+	let ret = [], l = elems.length;
+	while (l--) { ret[ret.length] = elems[l]; }
+	return ret;
+    })();
+ 
+    let shuffled = (function(){
+        let l = allElems.length, ret = [];
+        while (l--) {
+            let random = Math.floor(Math.random() * allElems.length),
+                randEl = allElems[random].cloneNode(true);
+            allElems.splice(random, 1);
+            ret[ret.length] = randEl;
+        }
+        return ret; 
+    })(), l = elems.length;
+ 
+    while (l--) {
+        elems[l].parentNode.insertBefore(shuffled[l], elems[l].nextSibling);
+        elems[l].parentNode.removeChild(elems[l]);
+    }
+ 
+}
+
+let button = document.querySelector('button');
+button.addEventListener('click', function() { shuffle( document.querySelectorAll('.deck > .card') ) }, false);
+/*fim da funcao embaralhar*/
